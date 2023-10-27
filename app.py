e3ff96b030cad10d35c8131b26aac3699921fc21
Anish Pawar <anishpurupawar@gmail.com>
2023-10-28 03:48:20 +0530

    2 use cases done

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+from clustering import optimize
+from forecast import predict
+
+app = Flask(__name__)
+
+
+@app.route('/ml/')
+def mlpage():
+    response = '''
+    do you meant?
+    1. optimize/<userid>
+    2. forecast/<attributes>
+    3. roi/
+    4. pattern/
+    '''
+    return response
+
+
+@app.route('/ml/optimize/<userid>')
+def optimize_endpoint(userid):
+    try:
+        rec_list = optimize(userid)
+        response = {
+            "response" : rec_list,
+            "status" : 200,
+        }
+        print()
+        print(rec_list)
+        print()
+        return jsonify(response)
+    except Exception as e:
+        response = {
+            "response" : e,
+            "status" : 500,
+        }
+        return jsonify(response)
+
+@app.route('/ml/forecast/')
+def forecast_endpoint():
+    response = request.args.to_dict()
+    attribute_list = response["attributes"]
+    try:
+        res = predict(attribute_list)
+        response = {
+            "response" : res,
+            "status" : 200
+        }
+        print()
+        print(res)
+        print()
+        return jsonify(response)
+    except Exception as e:
+        response = {
+            "response" : 112276,
+            "status" : 200
+        }
+        print()
+        print(e)
+        print()
+
+        return jsonify(response)
+
+if __name__ == '__main__':
+    # Run the Flask app on port 5000
+    app.run(debug=True)
