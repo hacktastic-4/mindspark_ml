e6e7798f232454655b9cdfef845f2c24d36bfeff
Anish Pawar <anishpurupawar@gmail.com>
2023-11-05 18:38:51 +0530

    cors enabled with updated req.txt

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, request, jsonify
 from clustering import optimize
 from forecast import predict
 from flask_cors import CORS
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route('/ml/')
+@cross_origin()
 def mlpage():
     response = '''
     do you meant?
@@ -18,6 +20,7 @@ def mlpage():
     return response
 
 @app.route('/ml/optimize/<userid>')
+@cross_origin()
 def optimize_endpoint(userid):
     try:
         rec_list = optimize(userid)
@@ -37,6 +40,7 @@ def optimize_endpoint(userid):
         return jsonify(response)
 
 @app.route('/ml/forecast/')
+@cross_origin()
 def forecast_endpoint():
     response = request.args.to_dict()
     attribute_list = response["attributes"]
